4ad736ec56ab1679f0ea2e12a263538921ce828e
Syed Hassan <hassan_baqar@outlook.com>
2022-04-02 18:20:21 +0500

    Update home.dart

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_qureos/authentication/authentication_cubit.dart';
 
 class Home extends StatelessWidget {
 
@@ -7,17 +9,22 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Dukkantek"),
-        automaticallyImplyLeading: false,
-      ),
-      body: Container(
-        color: Colors.black.withOpacity(0.8),
-        child: Center(child: Text("Welcome",
-        style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500,
-            color: Colors.white),)),
-      ),
+    return BlocBuilder<AuthenticationCubit, AuthenticationState>(
+        builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Dukkantek"),
+            automaticallyImplyLeading: false,
+          ),
+          body: Container(
+            color: Colors.black.withOpacity(0.8),
+            child: Center(child: Text(state.emailAddress == "" ? "Google signIn needs firebase linking and production SHA1 certificate" : "Hello ${state.emailAddress}",
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500,
+                color: Colors.white),
+            overflow: TextOverflow.ellipsis,)),
+          ),
+        );
+      }
     );
   }
 }
