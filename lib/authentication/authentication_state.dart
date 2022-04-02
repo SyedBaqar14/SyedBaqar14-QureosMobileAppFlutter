e39833ad3addc5dc3ce77bcf422cea1311cc0f27
Syed Hassan <hassan_baqar@outlook.com>
2022-04-02 18:21:27 +0500

    Update authentication_state.dart

--- lib/authentication/authentication_state.dart
+++ lib/authentication/authentication_state.dart
@@ -4,18 +4,23 @@ class AuthenticationState {
   bool isLoginSuccess;
   bool isloading;
   bool isObsecure;
+  String emailAddress;
 
   AuthenticationState({
+    required this.emailAddress,
     required this.isLoginSuccess,
     required this.isloading,
     required this.isObsecure});
 
   AuthenticationState copyWith({
+    String? emailAddress,
     bool? isLoginSuccess,
     bool? isloading,
     bool? isObsecure
 }) {
-    return AuthenticationState(isLoginSuccess: isLoginSuccess ?? this.isLoginSuccess,
+    return AuthenticationState(
+        emailAddress: emailAddress ?? this.emailAddress,
+        isLoginSuccess: isLoginSuccess ?? this.isLoginSuccess,
         isloading:  isloading ?? this.isloading,
         isObsecure: isObsecure ?? this.isObsecure);
   }
